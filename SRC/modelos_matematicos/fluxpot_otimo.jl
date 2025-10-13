41763f115d42ba80b85913e5dbf1b70ebc7a492e
Lucas Eduardo Silva Braga <62674895+lucasedbraga@users.noreply.github.com>
2025-10-13 15:55:30 -0300

    Update fluxpot_otimo.jl
    TESTE de alteracao via github

--- SRC/modelos_matematicos/fluxpot_otimo.jl
+++ SRC/modelos_matematicos/fluxpot_otimo.jl
@@ -23,6 +23,7 @@ println("=== OPF DC ITERATIVO COM PERDAS E CURTAILMENT ===")
 
 # Carrega dados da rede elétrica do arquivo JSON
 data = JSON.parsefile("DATA/input/3barras_BASE.json")
+#TESTE COMMIT
 #data = JSON.parsefile("../DATA/input/B6L8_BASE.json")
 #data = JSON.parsefile("../DATA/input/ieee14_BASE.json")
 #data = JSON.parsefile("DATA/input/ieee118_BASE.json")
